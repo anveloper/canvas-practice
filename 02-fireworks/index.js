56715702db59eed6b2d81b001b8b20c6a54c6ef0
anveloper <hitedin@gmail.com>
2023-04-23 23:31:31 +0900

    feat: 불꽃 꼬리 효과

--- 02-fireworks/index.js
+++ 02-fireworks/index.js
@@ -1,12 +1,13 @@
 import CanvasOption from "./js/CanvasOption.js";
 import Particle from "./js/Particle.js";
+import Tail from "./js/Tail.js";
 
-import { randomNumBetween } from "./js/utils.js";
+import { hypotenuse, randomNumBetween } from "./js/utils.js";
 
 class Canvas extends CanvasOption {
   constructor() {
     super();
-
+    this.tails = [];
     this.particles = [];
   }
   init() {
@@ -19,17 +20,28 @@ class Canvas extends CanvasOption {
 
     this.canvas.style.width = this.canvasWidth + "px";
     this.canvas.style.height = this.canvasHeight + "px";
+
     this.createParticles();
   }
 
-  createParticles() {
+  createTail() {
+    const x = randomNumBetween(this.canvasWidth * 0.2, this.canvasWidth * 0.8);
+    const vy = this.canvasHeight * randomNumBetween(0.01, 0.015) * -1;
+    const color = `255, 255, 255`;
+    this.tails.push(new Tail(x, vy, color));
+  }
+
+  createParticles(x, y, color) {
     const PARTICLE_NUM = 1000;
-    const x = randomNumBetween(0, this.canvasWidth);
-    const y = randomNumBetween(0, this.canvasHeight);
     for (let i = 0; i < PARTICLE_NUM; i++) {
-      const vx = randomNumBetween(-5, 5);
-      const vy = randomNumBetween(-5, 5);
-      this.particles.push(new Particle(x, y, vx, vy));
+      const r =
+        randomNumBetween(0, 100) * hypotenuse(innerWidth, innerHeight) * 0.0001;
+      const angle = (Math.PI / 180) * randomNumBetween(0, 360);
+
+      const vx = r * Math.cos(angle);
+      const vy = r * Math.sin(angle);
+      const opacity = randomNumBetween(0.6, 1.9);
+      this.particles.push(new Particle(x, y, vx, vy, opacity, color));
     }
   }
 
@@ -41,11 +53,21 @@ class Canvas extends CanvasOption {
       now = Date.now();
       delta = now - then;
       if (delta < this.interval) return;
-      this.ctx.fillStyle = this.bgColor;
+      then = now - (delta % this.interval);
+      this.ctx.fillStyle = this.bgColor + "30";
       this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
 
-      then = now - (delta % this.interval);
       // frame
+      if (Math.random() < 0.03) this.createTail();
+      this.tails.forEach((tail, index) => {
+        tail.update();
+        tail.draw();
+        if (tail.vy > -0.7) {
+          this.tails.splice(index, 1);
+          this.createParticles(tail.x, tail.y, tail.color);
+        }
+      });
+
       this.particles.forEach((particle, index) => {
         particle.update();
         particle.draw();

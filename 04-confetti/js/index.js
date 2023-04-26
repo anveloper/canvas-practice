3cbf60b9c09b346516548024b92606cc40dd677d
anveloper <hitedin@gmail.com>
2023-04-26 21:24:05 +0900

    feat: 콘페티 효과 구현완료

--- 04-confetti/js/index.js
+++ 04-confetti/js/index.js
@@ -17,20 +17,14 @@ function init() {
   canvas.width = canvasWidth * dpr;
   canvas.height = canvasHeight * dpr;
   ctx.scale(dpr, dpr);
-
-  confetti({
-    x: canvasWidth / 2,
-    y: canvasHeight / 2,
-    cnt: 10,
-  });
 }
 
-function confetti({ x, y, cnt }) {
+function confetti({ x, y, cnt, deg, colors, shapes, spread }) {
   for (let i = 0; i < cnt; i++) {
-    particles.push(new Particle(x, y));
+    particles.push(new Particle(x, y, deg, colors, shapes, spread));
   }
 }
-
+let deg = 0;
 function render() {
   let now, delta;
   let then = Date.now();
@@ -44,12 +38,54 @@ function render() {
     //
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
+    confetti({
+      x: 0,
+      y: 0.5,
+      cnt: 1,
+      deg: -50,
+    });
+    confetti({
+      x: 1,
+      y: 0.5,
+      cnt: 1,
+      deg: -130,
+    });
+    // confetti({
+    //   x: Math.random(),
+    //   y: Math.random(),
+    //   cnt: 3,
+    //   deg: 270,
+    //   spread: 180,
+    // });
+    // deg += 1;
+    // confetti({
+    //   x: 0.5,
+    //   y: 0.5,
+    //   cnt: 3,
+    //   deg: 225 + deg,
+    //   spread: 1,
+    // });
+    // confetti({
+    //   x: 0.5,
+    //   y: 0.5,
+    //   cnt: 3,
+    //   deg: 315 + deg,
+    //   spread: 1,
+    // });
+    // confetti({
+    //   x: 0.5,
+    //   y: 0.5,
+    //   cnt: 3,
+    //   deg: 90 + deg,
+    //   spread: 1,
+    // });
+
     for (let i = particles.length - 1; i >= 0; i--) {
       particles[i].update();
       particles[i].draw(ctx);
 
       if (particles[i].opacity < 0) particles.splice(i, 1);
-      if (particles[i].y > canvasHeight) particles.splice(i, 1);
+      if (particles[i]?.y > canvasHeight) particles.splice(i, 1);
     }
   };
 
@@ -60,4 +96,15 @@ window.addEventListener("resize", init);
 window.addEventListener("load", () => {
   init();
   render();
+});
+
+window.addEventListener("click", () => {
+  confetti({
+    x: 0,
+    y: 0.5,
+    cnt: 10,
+    deg: -50,
+    spread: 10,
+    // colors: ["#FF0000"],
+  });
 });
